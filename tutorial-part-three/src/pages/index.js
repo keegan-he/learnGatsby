6c2cd1ee13239fea323fd727f58957e68f01f6ba
Keegan H <keeganslane@gmail.com>
2020-03-30 22:00:31 -0700

    first commit

---
+++ tutorial-part-three/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Hello there world! Check me out! </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore molestiae
+      optio quos impedit enim vero perspiciatis id ex ipsum doloremque veritatis
+      officia ut eos at officiis, dolore quas cumque quo.
+    </p>
+  </Layout>
+)
